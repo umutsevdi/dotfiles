cd63b9d5290bc28da2c901a119b8e887549007a0
Umut Sevdi <sevdiumut@protonmail.com>
2022-07-04 19:28:46 +0300

    neovim migration to Lua

--- nvim/keybindings.lua
+++ nvim/keybindings.lua
@@ -1,6 +1,13 @@
---Shortcuts 
+--
+-- ╭───────────────────╮
+-- │  keybindings.lua  │
+-- ╰───────────────────╯
+--╭──────────────────────────────────────────────────────────────────────────────╮
+--│  keybindings.lua contains custom keybindings for various things.             │
+--╰──────────────────────────────────────────────────────────────────────────────╯
+-- @author umutsevdi
 
-vim.cmd [[
+vim.cmd([[
 " Go Compiler Settings
 autocmd BufWritePre *.go :silent call CocAction('runCommand',  'editor.action.organizeImport')
 autocmd FileType go nmap gtj :CocCommand go.tags.add json<cr>
@@ -9,103 +16,108 @@ autocmd FileType go nmap gtx :CocCommand go.tags.clear<cr>n
 
 " Window Movement
 " Change vim window focus
-map <A-h> <C-w>h
-map <A-l> <C-w>l
-map <A-j> <C-w>j
-map <A-k> <C-w>k
-map <A-Left> <C-w>h
-map <A-Down> <C-w>j
-map <A-Up> <C-w>k
-map <A-Right> <C-w>l
+map <C-h> <C-w>h
+map <C-l> <C-w>l
+map <C-j> <C-w>j
+map <C-k> <C-w>k
+map <C-Left> <C-w>h
+map <C-Down> <C-w>j
+map <C-Up> <C-w>k
+map <C-Right> <C-w>l
 
 " Tabs
 " Move around tabs
-noremap <silent> <C-h> :tabprevious<CR> 
-noremap <silent> <C-l> :tabnext<CR>
-noremap <silent> <C-Left> :tabprevious<CR>
-noremap <silent> <C-Right> :tabnext<CR>
-noremap <silent> <C-1> 1gt
-noremap <silent> <C-3> 3gt
-noremap <silent> <C-4> 4gt
-noremap <silent> <C-5> 5gt
-noremap <silent> <C-6> 6gt
-noremap <silent> <C-7> 7gt
-noremap <silent> <C-8> 8gt
-noremap <silent> <C-9> 9gt
-noremap <silent> <C-0> :tablast<cr>
+noremap <silent> <A-h> :tabprevious<CR> 
+noremap <silent> <A-l> :tabnext<CR>
+noremap <silent> <A-Left> :tabprevious<CR>
+noremap <silent> <A-Right> :tabnext<CR>
+noremap <silent> <A-1> 1gt
+noremap <silent> <A-2> 2gt
+noremap <silent> <A-3> 3gt
+noremap <silent> <A-4> 4gt
+noremap <silent> <A-5> 5gt
+noremap <silent> <A-6> 6gt
+noremap <silent> <A-7> 7gt
+noremap <silent> <A-8> 8gt
+noremap <silent> <A-9> 9gt
+noremap <silent> <A-0> :tablast<cr>
 
 " Jump definition
 nmap <silent> jd :call CocAction('jumpDefinition',  'split')<CR>
 nmap <silent> gd :call CocAction('jumpDefinition',  'vsplit')<CR>
 nmap <silent> gt :call CocAction('jumpDefinition',  'tabe')<CR>
-]]
+]])
 
 --quit without saving
-vim.keymap.set('n', "qq", ":q!<CR>")
+vim.keymap.set("n", "qq", ":q!<CR>")
 --quit after saving
-vim.keymap.set('n', "qw", ":wq<CR>")
+vim.keymap.set("n", "qw", ":wq<CR>")
 --source $MYVIMRC
-vim.keymap.set('n',  "<leader>ss", ":source $MYVIMRC<CR>")
+vim.keymap.set("n", "<leader>ss", ":source $MYVIMRC<CR>")
 -- tab management
-vim.keymap.set('n', "<silent> <C-n>", ":tabnew . <CR>")
-vim.keymap.set('n', "<A-n>", ":vsplit .<CR>")
+vim.keymap.set("n", "<A-n>", ":tabnew .<CR>")
+vim.keymap.set("n", "<A-q>", ":tabclose <CR>")
+vim.keymap.set("n", "<A-t>", ":vsplit .<CR>")
+vim.keymap.set("n", "<C-t>", ":vsplit .<CR>")
 
 --NerdTree
-vim.keymap.set('n', "<leader>n", ":NERDTreeFocus<CR>")
-vim.keymap.set('n', "<A-t>", ":NERDTreeToggle | TagbarToggle <CR>")
-vim.keymap.set('n', "<A-f>", ":NERDTreeFind<CR>")
+vim.keymap.set("n", "<leader>n", ":NERDTreeFocus<CR>")
+vim.keymap.set("n", "<A-Tab>", ":NERDTreeToggle | TagbarToggle <CR>")
+-- vim.keymap.set('n', "<A-f>", ":NERDTreeFind<CR>")
 
 --format code
-vim.keymap.set('n',  "<silent> <A-f>", ":Format <CR>")
+vim.keymap.set("n", "<A-f>", "<CR>   :Format <CR>")
 --Coc Diagnostic Menu
-vim.keymap.set('n',  "<silent> <A-d>", ":CocDiagnostics <CR>")
- 
+vim.keymap.set("n", "<silent> <A-d>", "<CR>  :CocDiagnostics <CR>")
+
 --Mappings for CoCList
 --Show all diagnostics.
-vim.keymap.set('n',  "<silent><nowait> <space>a", ":<C-u>CocList diagnostics<cr>")
+vim.keymap.set("n", "<A-d>", ":CocDiagnostics <cr>")
+
+vim.keymap.set("n", "<silent><nowait> <space>a", ":<C-u>CocList diagnostics<cr>")
 --Manage extensions.
-vim.keymap.set('n',  "<silent><nowait> <space>e", ":<C-u>CocList extensions<cr>")
+vim.keymap.set("n", "<silent><nowait> <space>e", ":<C-u>CocList extensions<cr>")
 --Show commands.
-vim.keymap.set('n',  "<silent><nowait> <space>c", ":<C-u>CocList commands<cr>")
+vim.keymap.set("n", "<silent><nowait> <space>c", ":<C-u>CocList commands<cr>")
 --Find symbol of current document.
-vim.keymap.set('n',  "<silent><nowait> <space>o", ":<C-u>CocList outline<cr>")
+vim.keymap.set("n", "<silent><nowait> <space>o", ":<C-u>CocList outline<cr>")
 --Search workspace symbols.
-vim.keymap.set('n',  "<silent><nowait> <space>s", ":<C-u>CocList -I symbols<cr>")
+vim.keymap.set("n", "<silent><nowait> <space>s", ":<C-u>CocList -I symbols<cr>")
 --Do default action for next item.
-vim.keymap.set('n',  "<silent><nowait> <space>j", ":<C-u>CocNext<CR>")
+vim.keymap.set("n", "<silent><nowait> <space>j", ":<C-u>CocNext<CR>")
 --Do default action for previous item.
-vim.keymap.set('n',  "<silent><nowait> <space>k", ":<C-u>CocPrev<CR>")
+vim.keymap.set("n", "<silent><nowait> <space>k", ":<C-u>CocPrev<CR>")
 --Resume latest coc list.
-vim.keymap.set('n',  "<silent><nowait> <space>p", ":<C-u>CocListResume<CR>")
+vim.keymap.set("n", "<silent><nowait> <space>p", ":<C-u>CocListResume<CR>")
 --Git Status
-vim.keymap.set('n',  "<silent> <space>g", ":<C-u>CocList --normal gstatus<CR>")
+vim.keymap.set("n", "<silent> <space>g", ":<C-u>CocList --normal gstatus<CR>")
 -- autofill
-vim.keymap.set('i', "\"", "\"\"<left>")
-vim.keymap.set('i', "\'", "\'\'<left>")
-vim.keymap.set('i', "(", "()<left>")
-vim.keymap.set('i', "[", "[]<left>")
-vim.keymap.set('i', "{", "{}<left>")
-vim.keymap.set('i', "(<CR>", "(<CR>)<ESC>O")
-vim.keymap.set('i', "(;<CR>", "(<CR>);<ESC>O")
-vim.keymap.set('i', "[<CR>", "[<CR>]<ESC>O")
-vim.keymap.set('i', "[;<CR>", "[<CR>];<ESC>O")
-vim.keymap.set('i', "{<CR>", "{<CR>}<ESC>O")
-vim.keymap.set('i', "{;<CR>", "{<CR>};<ESC>O")
-vim.keymap.set('i', "\"<CR>", "\"<CR>\"<ESC>O")
-vim.keymap.set('i', "\";<CR>", "\"<CR>\";<ESC>O")
-vim.keymap.set('i', "\'<CR>", "\'<CR>\'<ESC>O")
-vim.keymap.set('i', "\';<CR>", "\'<CR>\';<ESC>O")
+vim.keymap.set("i", '"', '""<left>')
+vim.keymap.set("i", "'", "''<left>")
+vim.keymap.set("i", "(", "()<left>")
+vim.keymap.set("i", "[", "[]<left>")
+vim.keymap.set("i", "{", "{}<left>")
+vim.keymap.set("i", "(<CR>", "(<CR>)<ESC>O")
+vim.keymap.set("i", "(;<CR>", "(<CR>);<ESC>O")
+vim.keymap.set("i", "[<CR>", "[<CR>]<ESC>O")
+vim.keymap.set("i", "[;<CR>", "[<CR>];<ESC>O")
+vim.keymap.set("i", "{<CR>", "{<CR>}<ESC>O")
+vim.keymap.set("i", "{;<CR>", "{<CR>};<ESC>O")
+vim.keymap.set("i", '"<CR>', '"<CR>"<ESC>O')
+vim.keymap.set("i", '";<CR>', '"<CR>";<ESC>O')
+vim.keymap.set("i", "'<CR>", "'<CR>'<ESC>O")
+vim.keymap.set("i", "';<CR>", "'<CR>';<ESC>O")
 --Telescope finder
 --Find files using Telescope command-line sugar.
 --vim.keymap.set('n',   <leader> <ff>: Telescope find_files<cr>
-vim.keymap.set('n',  'ff',':Telescope find_files <CR>')
+vim.keymap.set("n", "ff", ":Telescope find_files <CR>")
 --vim.keymap.set('n',  ff : Telescope find_files <CR>
-vim.keymap.set('n', "<leader>fg", "<cmd>Telescope live_grep<cr>")
-vim.keymap.set('n', "<leader>fb", "<cmd>Telescope buffers<cr>")
-vim.keymap.set('n', "<leader>fh", "<cmd>Telescope help_tags<cr>")
+vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
+vim.keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
 -- For C,  pressing K on the keyword will pull up the built-in manpage directly. For instance, place the cursor on the printf keyword:
 --Snippet runner
-vim.keymap.set('v',"rr",":SnipRun <CR>")
+vim.keymap.set("v", "rr", ":SnipRun <CR>")
 -- Better multiple lines tabbing with < and >
-vim.keymap.set('v',"<","<gv")
-vim.keymap.set('v',">",">gv")
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -20,7 +20,6 @@ require("pkg/nerdtree")
 require("pkg/tagbar")
 require("pkg/bracey")
 require("pkg/test")
-require("pkg/lightline")
 vim.cmd([[
     source $HOME/.dotfiles/nvim/pkg/coc.vim
     source $HOME/.dotfiles/nvim/pkg/startify.vim
@@ -37,10 +36,10 @@ call plug#begin()
     Plug 'preservim/nerdtree'                                                                " NERDTree is the file tree on right
     Plug 'nvim-lua/plenary.nvim'                                                             " Telescope requires this package
     Plug 'nvim-telescope/telescope.nvim'                                                     " Telescope is a FZF extension that displays preview
-    Plug 'itchyny/lightline.vim'                                                             " Lightline is the bar on the bottom that displays variues elements
-    Plug 'niklaas/lightline-gitdiff'                                                         " Lightline git extension
+    Plug 'nvim-lualine/lualine.nvim'                                                         " Lualine is the bar on the bottom that displays variues elements
     Plug 'mhinz/vim-startify'                                                                " Start page
     Plug 'tpope/vim-fugitive'                                                                " Git Integration
+    Plug 'gitgutter/Vim'                                                                     " Displays git diff
     Plug 'preservim/tagbar'                                                                  " Tag bar displays functions, classes and variables of files on the left 
     Plug 'nvim-treesitter/nvim-treesitter', {'do': ':TSUpdate'}                              " Syntax highlighting 
     Plug 'neoclide/coc.nvim', {'branch': 'release', 'do': { -> coc#util#install()}}          " Conqueror of Completions: Language server for any language
@@ -200,4 +199,91 @@ require("sniprun").setup({
 	borders = "single", -- display borders around floating windows
 	-- possible values are 'none', 'single', 'double', or 'shadow'
 	live_mode_toggle = "off", -- live mode toggle, see Usage - Running for more info
+})
+
+require("lualine").setup({
+	options = {
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {},
+		always_divide_middle = true,
+		globalstatus = false,
+	},
+	sections = {
+		lualine_a = { "mode", "branch" },
+		lualine_b = {
+			{
+				"filename",
+				file_status = true, -- Displays file status (readonly status, modified status)
+				path = 0, -- 0: Just the filename
+				-- 1: Relative path
+				-- 2: Absolute path
+				-- 3: Absolute path, with tilde as the home directory
+
+				shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+				-- for other components. (terrible name, any suggestions?)
+				symbols = {
+					modified = " * ", -- Text to show when the file is modified.
+					readonly = "  ", -- Text to show when the file is non-modifiable or readonly.
+					unnamed = "[No Name]", -- Text to show for unnamed buffers.
+				},
+			},
+		},
+		lualine_c = {
+			"CurrentFunction",
+			{
+				"diff",
+				colored = true, -- Displays a colored diff status if set to true
+				diff_color = {
+					-- Same color values as the general color option can be used here.
+					added = "DiffAdd", -- Changes the diff's added color
+					modified = "DiffChange", -- Changes the diff's modified color
+					removed = "DiffDelete", -- Changes the diff's removed color you
+				},
+				symbols = { added = "+", modified = "*", removed = "-" }, -- Changes the symbols used by the diff.
+				source = nil, -- A function that works as a data source for diff.
+				-- It must return a table as such:
+				--   { added = add_count, modified = modified_count, removed = removed_count }
+				-- or nil on failure. count <= 0 won't be displayed.
+			},
+		},
+		lualine_x = {
+			{
+				"diagnostics",
+				-- Table of diagnostic sources, available sources are:
+				--   'nvim_lsp', 'nvim_diagnostic', 'coc', 'ale', 'vim_lsp'.
+				-- or a function that returns a table as such:
+				--   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+				sources = { "nvim_diagnostic", "coc" },
+				-- Displays diagnostics for the defined severity types
+				sections = { "error", "warn", "info", "hint" },
+
+				diagnostics_color = {
+					-- Same values as the general color option can be used here.
+					error = "DiagnosticError", -- Changes diagnostics' error color.
+					warn = "DiagnosticWarn", -- Changes diagnostics' warn color.
+					info = "DiagnosticInfo", -- Changes diagnostics' info color.
+					hint = "DiagnosticHint", -- Changes diagnostics' hint color.
+				},
+				symbols = { error = " ⊗ ", warn = "⚠ ", info = "🛈 ", hint = " " },
+				colored = true, -- Displays diagnostics status in color if set to true.
+				update_in_insert = false, -- Update diagnostics in insert mode.
+				always_visible = false, -- Show diagnostics even if there are none.
+			},
+		},
+		lualine_y = { "location" },
+		lualine_z = { "encoding", "filetype" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { "filename" },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
 })
